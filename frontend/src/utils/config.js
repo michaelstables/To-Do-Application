6eaafb8674adce22e0a22c076796d9dceef21437
Michael Stables <michael.stables1991@gmail.com>
2024-01-18 12:03:39 -0600

    updating to use FQDN

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,1 @@
-export const API_PORT = 3002; // Change this to your desired API port
-export const API_URL = `https://todo.michaelstables.com:${API_PORT}`; // Use your actual domain or subdomain
-
-
+export const API_URL = `https://todo.michaelstables.com`; // No need to append the port if it's the standard 443 for HTTPS
